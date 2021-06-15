915b3465235bfadf412b9b198a4c8fa981c9bc0e
Shannon Denna <sdenna@d211.org>
2021-06-14 22:17:02 -0500

    Update Day1.java

--- src/examples/Day1.java
+++ src/examples/Day1.java
@@ -3,5 +3,6 @@ package examples;
 public class Day1 {
     public static void main(String[] args) {
         System.out.println("Hi Java - Day1");
+        System.out.println("On the RV");
     }
 }
